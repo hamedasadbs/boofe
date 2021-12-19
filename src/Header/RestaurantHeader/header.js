d33619a37aef28ebf9f84a030a8d02e648cf430d
hamedasad_bS <hamedasadollahi1998@gmail.com>
2021-12-19 16:10:04 +0330

    9/28

--- src/Header/RestaurantHeader/header.js
+++ src/Header/RestaurantHeader/header.js
@@ -7,11 +7,17 @@ import { Search } from "../Search/search";
 
 export const RestaurantHeader = () => (
   <div className={style.header}>
-    <div className={style.account}>
-      <img src={logo} alt="logo" />
-      <h1>حامد اسداللهی</h1>
-      <i className="fa fa-user"></i>
-    </div>
+    <main>
+      <div className={style.logo}>
+        <a href="/">
+          <img src={logo} alt="logo" />
+        </a>
+      </div>
+      <a href="/sign" className={style.account}>
+        <h1>حامد اسداللهی</h1>
+        <i className="fa fa-user"></i>
+      </a>
+    </main>
     <Search />
   </div>
 );
