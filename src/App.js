137ff0bffe03538acc0ef58ca460ed73e44555de
hamedasad_bS <hamedasadollahi1998@gmail.com>
2022-01-12 19:25:02 +0330

    10/22

--- src/App.js
+++ src/App.js
@@ -82,7 +82,7 @@ export const App = () => {
               </>
             )}
           </Route>
-          <Route path="https://hamedasadbs.github.io/boofe/">
+          <Route path="/">
             {getCookie("role") == "1" ? (
               <Redirect to="/admin" />
             ) : (
